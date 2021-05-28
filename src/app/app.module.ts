ccec8b64a40a2e52c4b1dfd6c1680f63cd1dc9df
osiris <wendyosi25@gmail.com>
2021-05-28 11:09:52 -0500

    feat/component creation login,products and customers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './components/login/login.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { CustomersComponent } from './components/customers/customers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProductsComponent,
+    ClientsComponent,
+    CustomersComponent
   ],
   imports: [
     BrowserModule,
