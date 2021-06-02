f1a49058ab827d25959e0b1fe67142848a44779f
Talia Gonzalez <triptofanita@Talias-MacBook-Pro.local>
2021-06-02 02:31:16 -0500

    add opportunity detail component

---
+++ src/app/components/opportunity/opportunity-detail/opportunity-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-opportunity-detail',
+  templateUrl: './opportunity-detail.component.html',
+  styleUrls: ['./opportunity-detail.component.css']
+})
+export class OpportunityDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
