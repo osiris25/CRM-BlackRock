6a3a1828801eedcbd4fe80b462d440d5990923ef
osiris <wendyosi25@gmail.com>
2021-06-02 00:44:50 -0500

    (feat/pdf)pdf generation without styles

--- src/app/components/report/report.component.ts
+++ src/app/components/report/report.component.ts
@@ -1,24 +1,77 @@
-import { Component, OnInit } from '@angular/core';
-import jspdf from 'jspdf';
-
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
   styleUrls: ['./report.component.css']
 })
-export class ReportComponent implements OnInit {
+export class ReportComponent  {
+	VIDEOGAMES = [
+    {
+      id: 1,
+      name: 'Animal Crossing',
+      platform: 'Nintendo Switch',
+      reference: '1-770-736-8031',
+    },
+    {
+      id: 2,
+      name: 'The Legend of Zelda: Ocarina of Time CV',
+      platform: 'Wii U',
+      reference: '1-770-736-2323',
+    },
+    {
+      id: 3,
+      name: 'Metal Gear Solid',
+      platform: 'Playstation (PSX)',
+      reference: '1-4564-736-334',
+    },
+    {
+      id: 4,
+      name: 'ShenMue',
+      platform: 'Sega Dreamcast',
+      reference: '3-770-736-4532',
+    },
+    {
+      id: 5,
+      name: 'Rise of the Tomb Raider',
+      platform: 'Playstation 4',
+      reference: '1-324-736-3245',
+    },
+    {
+      id: 6,
+      name: 'Resident Evil 2',
+      platform: 'Playstation',
+      reference: '1-123-3336-4321',
+    },
+  ];
+	constructor() {
+		this.generarPDF();
+	 }
 
-	generarPDF(){
-		var div = document.getElementById("tabla");
-		var pdf = new jspdf();
-		pdf.text("documento generado por jspdf",10,10);
-		// pdf.fromHTML(div,30,30);
-		pdf.save("reporte.pdf");
+		public generarPDF():void{
+			// Extraemos el
+    const DATA = document.getElementById('htmlData') as HTMLElement;
+    const doc = new jsPDF('p', 'pt', 'a4');
+    const options = {
+      background: 'white',
+      scale: 3
+    };
+    html2canvas(DATA, options).then((canvas) => {
+
+      const img = canvas.toDataURL('image/PNG');
+
+      // Add image Canvas to PDF
+      const bufferX = 15;
+      const bufferY = 15;
+      const imgProps = (doc as any).getImageProperties(img);
+      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+      return doc;
+    }).then((docResult) => {
+      docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
+    });
+  };
 	}
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
